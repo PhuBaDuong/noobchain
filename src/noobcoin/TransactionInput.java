ecb80bb754b4432fe6f92eae1542e80101c7405f
Phu Duong <phubaduong@gmail.com>
2018-06-11 22:44:25 +0700

    [blockchain-03] update  code

--- src/noobcoin/TransactionInput.java
+++ src/noobcoin/TransactionInput.java
@@ -2,4 +2,12 @@ package noobcoin;
 
 public class TransactionInput {
 
+	public String transactionOutputId;
+	public TransactionOutput UTXO;
+
+	public TransactionInput(String transactionOutputId) {
+		super();
+		this.transactionOutputId = transactionOutputId;
+	}
+
 }
--- src/noobcoin/TransactionOutput.java
+++ src/noobcoin/TransactionOutput.java
@@ -1,5 +1,22 @@
 package noobcoin;
 
+import java.security.PublicKey;
+
 public class TransactionOutput {
 
+	public String id;
+	public PublicKey reciepient;
+	public float value;
+	public String parentTransactionId;
+
+	public TransactionOutput(PublicKey reciepient, float value, String parentTransactionId) {
+		super();
+		this.reciepient = reciepient;
+		this.value = value;
+		this.parentTransactionId = parentTransactionId;
+	}
+
+	public boolean isMine(PublicKey publicKey) {
+		return this.reciepient == publicKey;
+	}
 }
